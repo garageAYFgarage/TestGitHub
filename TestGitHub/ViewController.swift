946875eb440807da6371104b770c5f88a0911d20
ANDREW FARAFONgarageAYFgarage <ay.farafon@gmail.com>
2022-11-28 17:20:04 +0200

    clean project

--- TestGitHub/ViewController.swift
+++ TestGitHub/ViewController.swift
@@ -13,12 +13,6 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-    //first comment
-    //second commit
-    //third
-    //fourth
-    //fifth
-
+    
 }
 
